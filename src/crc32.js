bilibili-helper/bilibili-helper-o

commit 49611970f4093c6c4148e633a4e2be1bb33b7a04

Fix comment sender lookup. Resolve #406.

src/crc32.js
@@ -1,0 +1,118 @@
+/* eslint no-unused-vars: 0 */
+/* global Uint32Array: false */
+
+const CRC32_POLY = 0xEDB88320;
+
+class CrcRainbowTableNode {
+    constructor(currDigit = '') {
+        this.val = currDigit;
+        this.next = [];
+    }
+}
+
+class Crc32Engine {
+    static initCrc32Table(table) {
+        for (let i = 0; i < 256; i++) {
+            let currCrc = i;
+            for (let j = 0; j < 8; j++) {
+                if (currCrc & 1) {
+                    currCrc = (currCrc >>> 1) ^ CRC32_POLY;
+                } else {
+                    currCrc >>>= 1;
+                }
+            }
+            table[i] = currCrc;
+        }
+    }
+
+    constructor() {
+        this.crc32Table = new Uint32Array(256);
+        Crc32Engine.initCrc32Table(this.crc32Table);
+        this.rainbowTable = new CrcRainbowTableNode();
+        // Initialize the rainbow Table
+        for (let i = 1; i < 100000; i++) {
+            this.addRainbowTableEntry(i);
+        }
+    }
+
+    addRainbowTableEntry(i) {
+        let currNode = this.rainbowTable;
+        let hash = (this.compute(i) >>> 0).toString(16);
+        for (let currChar of hash) {
+            let hasChar = false;
+            for (let child of currNode.next) {
+                if (child.val === currChar) {
+                    currNode = child;
+                    hasChar = true;
+                    break;
+                }
+            }
+            if (!hasChar) {
+                currNode.next.push(new CrcRainbowTableNode(currChar));
+                currNode = currNode.next[currNode.next.length - 1];
+            }
+        }
+        currNode.next.push(i);
+    }
+
+    compute(input, addPadding = false) {
+        let currCrc = 0;
+        for (let digit of input.toString()) {
+            currCrc = this.crc32Update(currCrc, Number(digit));
+        }
+        if (addPadding) {
+            for (let i = 0; i < 5; i++) {
+                currCrc = this.crc32Update(currCrc, 0);
+            }
+        }
+        return currCrc;
+    }
+
+    crack(hash) {
+        let results = [];
+        let hashVal = ~Number('0x' + hash) >>> 0;
+        let baseHash = 0xFFFFFFFF;
+
+        for (let digitCount = 1; digitCount < 10; digitCount++) {
+            baseHash = this.crc32Update(baseHash, 0x30); // 0x30: '0'
+            if (digitCount < 6) {
+                // Direct lookup
+                results = results.concat(this.lookup(hashVal ^ baseHash));
+            } else {
+                // Lookup with prefix
+                let startPrefix = Math.pow(10, digitCount - 6);
+                let endPrefix = Math.pow(10, digitCount - 5);
+
+                for (let prefix = startPrefix; prefix < endPrefix; prefix++) {
+                    for (let postfix of this.lookup(hashVal ^ baseHash ^
+                                                    this.compute(prefix, true))) {
+                        results.push(prefix * 100000 + postfix);
+                    }
+                }
+            }
+        }
+        return results;
+    }
+
+    crc32Update(currCrc, code) {
+        return (currCrc >>> 8) ^ this.crc32Table[(currCrc ^ code) & 0xFF];
+    }
+
+    lookup(hash) {
+        let currNode = this.rainbowTable;
+        for (let currChar of (hash >>> 0).toString(16)) {
+            let hasChar = false;
+            for (let child of currNode.next) {
+                if (child.val === currChar) {
+                    currNode = child;
+                    hasChar = true;
+                    break;
+                }
+            }
+            if (!hasChar) {
+                return [];
+            }
+        }
+        return currNode.next.filter((i) => typeof i === 'number');
+    }
+}

LICENSE
@@ -1,5 +1,5 @@
 The MIT License (MIT)
-Copyright (c) 2015 Zac Yu <zacyu@google.com>, Google Inc., ruo <jjj201200@gmail.com>
+Copyright (c) 2018 Zac Yu <zacyu@google.com>, Google Inc., ruo <jjj201200@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 

src/background.html
@@ -1,7 +1,8 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <script type="text/javascript" src="resource.js"></script>
-    <script type="text/javascript" src="background.js"></script>
+    <script type="text/javascript" src="resource.min.js"></script>
+    <script type="text/javascript" src="crc32.min.js"></script>
+    <script type="text/javascript" src="background.min.js"></script>
 </head>
 </html>

src/background.js
@@ -1,5 +1,5 @@
 /* eslint no-unused-vars: 0 */
-/* global setOption: false, getOption: false, getCSS: false, version: false */
+/* global setOption: false, getOption: false, getCSS: false, version: false, Crc32Engine: false */
 
 let notification = false,
     notificationAvid = {},
@@ -17,7 +17,8 @@ let notification = false,
     bangumi = false,
     CRSF, watchLater = false,
     hasLogin = false,
-    subName = '/neptune/';
+    subName = '/neptune/',
+    crcEngine = new Crc32Engine();
 
 
 Live.set = function(n, k, v) {
@@ -852,6 +853,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         downloadNames[request.url.split('?')[0]] = request.filename;
         sendResponse();
         return false;
+    case 'uidLookup':
+        sendResponse({
+            uids: crcEngine.crack(request.user),
+        });
+        return true;
     default:
         sendResponse({
             result: 'unknown',
@@ -1078,7 +1084,6 @@ chrome.webRequest.onHeadersReceived.addListener(function(details) {
             value: 'attachment; filename*=UTF-8\'\'' + encodeURIComponent(downloadNames[baseUri]),
         });
     }
-    console.warn(modifiedHeaders);
     return {
         responseHeaders: modifiedHeaders,
     };

src/bilibili_injected.js
@@ -183,14 +183,14 @@
             command: 'getAd',
         }, function(response) {
             if (response.value === 'on') {
-                inject_css('bilibiliHelperAdStyle', 'bilibiliHelperAd.css');
+                inject_css('bilibiliHelperAdStyle', 'bilibiliHelperAd.min.css');
             }
         });
     }
     removeAd();
 
     function initStyle() {
-        inject_css('bilibiliHelperVideo', 'bilibiliHelperVideo.css');
+        inject_css('bilibiliHelperVideo', 'bilibiliHelperVideo.min.css');
         $('.arc-toolbar .helper .t .icon').css('background-image', 'url(' + chrome.extension.getURL('imgs/helper-neko.png') + ')');
     }
 
@@ -729,31 +729,36 @@
                             let displayUserInfo = function(uid, data) {
                                 biliHelper.comments[index].senderId = uid;
                                 biliHelper.comments[index].senderUsername = parseSafe(data.name);
-                                control.find('.result').html('发送者: <a href="' + biliHelper.protocol + '//space.bilibili.com/' + uid + '" target="_blank" data-usercard-mid="' + uid + '">' + parseSafe(data.name) + '</a><div target="_blank" class="user-info-level l' + parseSafe(data.level_info.current_level) + '"></div>');
+                                control.find('.result span a[data-usercard-mid="' + uid + '"]').text(data.name).after('<div target="_blank" class="user-info-level l' + parseSafe(data.level_info.current_level) + '"></div>');
                                 let s = document.createElement('script');
                                 s.appendChild(document.createTextNode('UserCard.bind($("#bilibili_helper .query .result"));'));
                                 document.body.appendChild(s);
                                 s.parentNode.removeChild(s);
                             };
 
-                            let renderSender = function(uid) {
-                                control.find('.result').html('发送者 UID: <a href="' + biliHelper.protocol + '//space.bilibili.com/' + uid + '" target="_blank">' + uid + '</a>');
-                                let cachedData = sessionStorage.getItem('user/' + uid);
-                                if (cachedData) {
-                                    displayUserInfo(uid, JSON.parse(cachedData));
-                                } else {
-                                    $.getJSON(biliHelper.protocol + '//api.bilibili.com/cardrich?mid=' + uid + '&type=json', function(data) {
-                                        if (data.code === 0) {
-                                            let cardData = data.data.card;
-                                            sessionStorage.setItem('user/' + uid, JSON.stringify({
-                                                name: cardData.name,
-                                                level_info: {
-                                                    current_level: cardData.level_info.current_level,
-                                                },
-                                            }));
-                                            displayUserInfo(uid, cardData);
-                                        }
-                                    });
+                            let renderSender = function(uids) {
+                                control.find('.result').html('发送者: <span></span>');
+                                for (let uid of uids) {
+                                    control.find('.result span').append('<a href="' + biliHelper.protocol + '//space.bilibili.com/' + uid + '" target="_blank" data-usercard-mid="' + uid + '">UID: ' + uid + '</a><br/>');
+                                    let cachedData = sessionStorage.getItem('user/' + uid);
+                                    if (cachedData) {
+                                        displayUserInfo(uid, JSON.parse(cachedData));
+                                    } else {
+                                        $.getJSON(biliHelper.protocol + '//api.bilibili.com/cardrich?mid=' + uid + '&type=json', function(data) {
+                                            if (data.code === 0) {
+                                                let cardData = data.data.card;
+                                                sessionStorage.setItem('user/' + uid, JSON.stringify({
+                                                    name: cardData.name,
+                                                    level_info: {
+                                                        current_level: cardData.level_info.current_level,
+                                                    },
+                                                }));
+                                                displayUserInfo(uid, cardData);
+                                            } else if (data.code === -626) {
+                                                control.find('.result span a[data-usercard-mid="' + uid + '"],.result span a[data-usercard-mid="' + uid + '"]+br').remove();
+                                            }
+                                        });
+                                    }
                                 }
                             };
 
@@ -761,16 +766,17 @@
                             if (extracted) {
                                 renderSender(extracted[1]);
                             } else {
-                                $.get('https://biliquery.typcn.com/api/user/hash/' + sender, function(data) {
-                                    if (!data || data.error !== 0 || typeof data.data !== 'object' || !data.data[0].id) {
-                                        control.find('.result').text('查询失败, 发送用户可能已被管理员删除.');
+                                chrome.runtime.sendMessage({
+                                    command: 'uidLookup',
+                                    user: sender,
+                                }, function(result) {
+                                    if (result.uids.length < 0) {
+                                        control.find('.result').text('查询失败.');
                                         item.addClass('error');
                                         biliHelper.comments[index].error = true;
                                     } else {
-                                        renderSender(data.data[0].id);
+                                        renderSender(result.uids);
                                     }
-                                }, 'json').fail(function() {
-                                    control.find('.result').text('查询失败, 无法连接到服务器 :(');
                                 });
                             }
                         });

src/options.html
@@ -5,9 +5,9 @@
         <meta charset="UTF-8">
         <title>哔哩哔哩助手 - 选项页</title>
         <link rel="stylesheet" type="text/css" href="sweetalert.min.css">
-        <link rel="stylesheet" type="text/css" href="options.css">
+        <link rel="stylesheet" type="text/css" href="options.min.css">
         <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
-        <script type="text/javascript" src="options.js"></script>
+        <script type="text/javascript" src="options.min.js"></script>
         <script type="text/javascript" src="sweetalert.min.js"></script>
     </head>
 
@@ -218,8 +218,9 @@
                 <p>Google商店下载地址: <a href="https://chrome.google.com/webstore/detail/kpbnombpnpcffllnianjibmpadjolanh" target="_blank">哔哩哔哩助手：bilibili.com 综合辅助扩展</a></p>
                 <p>Github下载地址: <a href="Download at Github" target="_blank">Download at Github</a></p>
                 <br/>
-                <p>※ 本页面中使用了 t4t5 以 <a href="http://opensource.org/licenses/MIT" target="_blank">MIT许可协议</a> 发布的 JavaScript 脚本 <a href="https://github.com/t4t5/sweetalert" target="_blank">sweetalert</a>。</p>
-                <p>※ 本项目中使用的 ASS 格式弹幕转换脚本修改自 tiansh 以 <a href="http://www.mozilla.org/MPL/2.0/" target="_blank">Mozilla Public License 2.0</a>                发布的开源项目 <a href="https://github.com/tiansh/us-danmaku" target="_blank">us-danmaku</a>。</p>
+                <p>※ 本页面中使用了 t4t5 以 <a href="http://opensource.org/licenses/MIT" target="_blank">MIT 许可协议</a> 发布的 JavaScript 脚本 <a href="https://github.com/t4t5/sweetalert" target="_blank">sweetalert</a>。</p>
+                <p>※ 本项目中使用的 ASS 格式弹幕转换脚本修改自 tiansh 以 <a href="http://www.mozilla.org/MPL/2.0/" target="_blank">Mozilla Public License 2.0</a> 发布的开源项目 <a href="https://github.com/tiansh/us-danmaku" target="_blank">us-danmaku</a>。</p>
+                <p>※ 本项目中使用的本地 CRC32 反查脚本修改自 xmcp 以 <a href="http://www.mozilla.org/MPL/2.0/" target="_blank">GPL 许可协议</a> 发布的开源项目 <a href="https://github.com/xmcp/pakku.js" target="_blank">pakku.js</a> 中的部分代码。</p>
                 <p>咕咕投喂宝暂时无法使用，所以更换成肉肉的。欢迎喂食 _(:3」∠)_</p>
                 <img class="alipay" src="./imgs/alipay.png">
             </div>

src/popup.html
@@ -5,9 +5,9 @@
         <meta charset="UTF-8">
         <title>哔哩哔哩助手 - POPUP</title>
         <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
-        <script type="text/javascript" src="resource.js"></script>
-        <script type="text/javascript" src="popup.js"></script>
-        <link rel="stylesheet" type="text/css" href="popup.css">
+        <script type="text/javascript" src="resource.min.js"></script>
+        <script type="text/javascript" src="popup.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="popup.min.css">
 
     </head>
 

commit efc2e67d83a49555e07a2d8424b799ef8da5f554

Change CRC32 Engine implementation to save memory.
The new implementation now uses about 1 MB of memory.
The issue was introduced in 49611970f4093c6c4148e633a4e2be1bb33b7a04 as
an attempt to fix #406, and was first addressed in #423.

src/crc32.js
@@ -3,13 +3,6 @@
 
 const CRC32_POLY = 0xEDB88320;
 
-class CrcRainbowTableNode {
-    constructor(currDigit = '') {
-        this.val = currDigit;
-        this.next = [];
-    }
-}
-
 class Crc32Engine {
     static initCrc32Table(table) {
         for (let i = 0; i < 256; i++) {
@@ -28,31 +21,23 @@ class Crc32Engine {
     constructor() {
         this.crc32Table = new Uint32Array(256);
         Crc32Engine.initCrc32Table(this.crc32Table);
-        this.rainbowTable = new CrcRainbowTableNode();
+        this.rainbowTableHash = new Uint32Array(100000);
+        this.rainbowTableValue = new Uint32Array(100000);
+        let fullHashCache = new Uint32Array(100000),
+            shortHashBuckets = new Uint32Array(65537);
         // Initialize the rainbow Table
-        for (let i = 1; i < 100000; i++) {
-            this.addRainbowTableEntry(i);
+        for (let i = 0; i < 100000; i++) {
+            let hash = this.compute(i) >>> 0;
+            fullHashCache[i] = hash;
+            shortHashBuckets[hash >>> 16]++;
         }
-    }
-
-    addRainbowTableEntry(i) {
-        let currNode = this.rainbowTable;
-        let hash = (this.compute(i) >>> 0).toString(16);
-        for (let currChar of hash) {
-            let hasChar = false;
-            for (let child of currNode.next) {
-                if (child.val === currChar) {
-                    currNode = child;
-                    hasChar = true;
-                    break;
-                }
-            }
-            if (!hasChar) {
-                currNode.next.push(new CrcRainbowTableNode(currChar));
-                currNode = currNode.next[currNode.next.length - 1];
-            }
+        let runningSum = 0;
+        this.shortHashBucketStarts = shortHashBuckets.map((n) => runningSum += n);
+        for (let i = 0; i < 100000; i++) {
+            let idx = --this.shortHashBucketStarts[fullHashCache[i] >>> 16];
+            this.rainbowTableHash[idx] = fullHashCache[i];
+            this.rainbowTableValue[idx] = i;
         }
-        currNode.next.push(i);
     }
 
     compute(input, addPadding = false) {
@@ -69,7 +54,7 @@ class Crc32Engine {
     }
 
     crack(hash) {
-        let results = [];
+        let candidates = [];
         let hashVal = ~Number('0x' + hash) >>> 0;
         let baseHash = 0xFFFFFFFF;
 
@@ -77,7 +62,7 @@ class Crc32Engine {
             baseHash = this.crc32Update(baseHash, 0x30); // 0x30: '0'
             if (digitCount < 6) {
                 // Direct lookup
-                results = results.concat(this.lookup(hashVal ^ baseHash));
+                candidates = candidates.concat(this.lookup(hashVal ^ baseHash));
             } else {
                 // Lookup with prefix
                 let startPrefix = Math.pow(10, digitCount - 6);
@@ -86,12 +71,12 @@ class Crc32Engine {
                 for (let prefix = startPrefix; prefix < endPrefix; prefix++) {
                     for (let postfix of this.lookup(hashVal ^ baseHash ^
                                                     this.compute(prefix, true))) {
-                        results.push(prefix * 100000 + postfix);
+                        candidates.push(prefix * 100000 + postfix);
                     }
                 }
             }
         }
-        return results;
+        return candidates;
     }
 
     crc32Update(currCrc, code) {
@@ -99,20 +84,15 @@ class Crc32Engine {
     }
 
     lookup(hash) {
-        let currNode = this.rainbowTable;
-        for (let currChar of (hash >>> 0).toString(16)) {
-            let hasChar = false;
-            for (let child of currNode.next) {
-                if (child.val === currChar) {
-                    currNode = child;
-                    hasChar = true;
-                    break;
-                }
-            }
-            if (!hasChar) {
-                return [];
+        hash >>>= 0;
+        let candidates = [];
+        let shortHash = hash >>> 16;
+        for (let i = this.shortHashBucketStarts[shortHash];
+             i < this.shortHashBucketStarts[shortHash + 1]; i++) {
+            if (this.rainbowTableHash[i] === hash) {
+                candidates.push(this.rainbowTableValue[i]);
             }
         }
-        return currNode.next.filter((i) => typeof i === 'number');
+        return candidates;
     }
 }