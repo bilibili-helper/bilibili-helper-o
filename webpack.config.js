59190252564625fb7db56f022fa0e1e8cce1f42a
DrowsyFlesh <jjj201200@gmail.com>
2018-11-18 03:09:07 +0800

    rename webpack-src to src

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MergeJsonWebpackPlugin = require('merge-jsons-webpack-plugin');
 
-const webpackPath = path.resolve('webpack-src');
+const srcPath = path.resolve('src');
 const buildPath = path.resolve('build');
-const webpackJSPath = path.resolve(webpackPath, 'js');
+const jsPath = path.resolve(srcPath, 'js');
 const indexFilename = 'index.js';
 
 const localesSupportList = require('./localesSupportList.json');
 const localesGroup = localesSupportList.map((name) => ({
-    pattern: `{./webpack-src/**/_locales/${name}/messages.json,./webpack-src/_locales/${name}/messages.json}`,
+    pattern: `{./src/**/_locales/${name}/messages.json,./src/_locales/${name}/messages.json}`,
     fileName: `./_locales/${name}/messages.json`,
 }));
 
@@ -37,11 +37,11 @@ module.exports = {
         ignored: ['node_modules'],
     },
     entry: {
-        'background': path.resolve(webpackJSPath, 'pages', 'background', indexFilename),
-        'live': path.resolve(webpackJSPath, 'pages', 'live', indexFilename),
-        'config': path.resolve(webpackJSPath, 'pages', 'config', indexFilename),
-        'popup': path.resolve(webpackJSPath, 'pages', 'popup', indexFilename),
-        'video': path.resolve(webpackJSPath, 'pages', 'video', indexFilename),
+        'background': path.resolve(jsPath, 'pages', 'background', indexFilename),
+        'live': path.resolve(jsPath, 'pages', 'live', indexFilename),
+        'config': path.resolve(jsPath, 'pages', 'config', indexFilename),
+        'popup': path.resolve(jsPath, 'pages', 'popup', indexFilename),
+        'video': path.resolve(jsPath, 'pages', 'video', indexFilename),
     },
     output: {
         filename: '[name].js',
@@ -59,12 +59,12 @@ module.exports = {
     },
     resolve: {
         alias: {
-            'Libs': path.resolve(webpackJSPath, 'libs'),
-            'Utils': path.resolve(webpackJSPath, 'utils'),
-            'Components': path.resolve(webpackJSPath, 'components'),
-            'Modules': path.resolve(webpackJSPath, 'modules'),
-            'Statics': path.resolve(webpackPath, 'statics'),
-            'Styles': path.resolve(webpackPath, 'styles'),
+            'Libs': path.resolve(jsPath, 'libs'),
+            'Utils': path.resolve(jsPath, 'utils'),
+            'Components': path.resolve(jsPath, 'components'),
+            'Modules': path.resolve(jsPath, 'modules'),
+            'Statics': path.resolve(srcPath, 'statics'),
+            'Styles': path.resolve(srcPath, 'styles'),
         },
         mainFiles: ['index.js'],
         extensions: ['.js', '.json', '.jsx', '.css', '.less', '.scss', '.sass'],
@@ -94,10 +94,10 @@ module.exports = {
             chunkFilename: path.join('styles', '[id].css'),
         }),
         new CopyWebpackPlugin([
-            {from: 'webpack-src/html/*.html', to: '', flatten: true},
-            {from: 'webpack-src/*.json', to: '', flatten: true},
-            {from: 'webpack-src/statics', to: 'statics'},
-            {from: 'webpack-src/js/libs', to: 'libs'},
+            {from: 'src/html/*.html', to: '', flatten: true},
+            {from: 'src/*.json', to: '', flatten: true},
+            {from: 'src/statics', to: 'statics'},
+            {from: 'src/js/libs', to: 'libs'},
             // {from: 'webpack-src/styles/**/*.css', to: 'styles/css', flatten: true},
         ]),
         new MergeJsonWebpackPlugin({
