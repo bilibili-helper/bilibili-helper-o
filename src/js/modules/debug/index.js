3a2342d55ea5b8d1bfc9d75851714fa3b1553560
DrowsyFlesh <jjj201200@gmail.com>
2018-11-20 06:19:07 +0800

    update

--- src/js/modules/debug/index.js
+++ src/js/modules/debug/index.js
@@ -12,8 +12,8 @@ export class Debug extends Feature {
             name: 'debug',
             kind: 'other',
             settings: {
-                on: true,
-                toggle: false,
+                on: false,
+                toggle: true,
                 title: '调试模式',
                 description: '开启后会在控制台输出更多信息，并执行更多用于测试的代码',
             },
