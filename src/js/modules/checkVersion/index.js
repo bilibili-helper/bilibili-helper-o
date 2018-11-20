d5f6eb4d3aa5ea9a5475e27e9db37999f1493baa
DrowsyFlesh <jjj201200@gmail.com>
2018-11-20 17:46:48 +0800

    clean up code

--- src/js/modules/checkVersion/index.js
+++ src/js/modules/checkVersion/index.js
@@ -35,13 +35,10 @@ export class CheckVersion extends Feature {
             method: 'get',
             url: `https://bilihelper.guguke.net/version.json`,
             success: (res) => {
-                let version = '0.9.8';
                 const {day} = this.getVersion();
                 if (this.compareVersion(res.version, version) > 0 && day !== new Date().getDate()) { // 比较今天是否有检测过
                     this.setVersion(res.version);
                     const notifyOn = _.find(this.settings.options, (o) => o.key === 'notification').on;
-
-                    console.log(__('extensionNotificationTitle'), __('checkVersionNewVersion'));
                     notifyOn && chrome.notifications.create(`bh-${this.name}-${(Math.random() * 1000).toFixed(0)}`, {
                         type: 'basic',
                         iconUrl: getURL('/statics/imgs/cat.svg'),
