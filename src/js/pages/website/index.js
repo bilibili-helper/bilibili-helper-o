aa8197dc19b384b558721bb263de0b3a2a58135c
DrowsyFlesh <jjj201200@gmail.com>
2019-03-08 23:37:00 +0800

    update

--- src/js/pages/website/index.js
+++ src/js/pages/website/index.js
@@ -4,19 +4,27 @@
  * Create: 2019/2/27
  * Description:
  */
-const HelperID = 'kpbnombpnpcffllnianjibmpadjolanh';
-let HelperPort = chrome.runtime.connect(HelperID);
-// 接收来自proxy for website 模块的message并转发给website
-HelperPort.onMessage.addListener((message /*HelperPort*/) => {
-    const {from, ...rest} = message;
-    if (from !== 'helperProxy') { return; }
-    window.postMessage({from: 'helper', ...rest}, TARGET_ORIGIN);
-});
+const createPort = () => {
+    const HelperID = 'kpbnombpnpcffllnianjibmpadjolanh';
+    let port = chrome.runtime.connect(HelperID);
+    // 接收来自proxy for website 模块的message并转发给website
+    port.onMessage.addListener((message /*HelperPort*/) => {
+        const {from, ...rest} = message;
+        if (from !== 'helperProxy') { return; }
+        window.postMessage({from: 'helper', ...rest}, TARGET_ORIGIN);
+    });
+    port.onDisconnect.addListener(() => {
+        port = null;
+    });
+    return port;
+};
 
 // 接收从website代码发送过来的message并转发给helper proxy for website 模块
+let HelperPort;
 window.addEventListener('message', (event) => {
     const {from, ...rest} = event.data;
     if (from !== 'website') { return; }
+    if (!HelperPort) { HelperPort = createPort(); }
     HelperPort.postMessage({from, ...rest});
 });
 
--- src/styles/globalInject.js
+++ src/styles/globalInject.js
@@ -6,15 +6,12 @@
 
 import {injectGlobal} from 'styled-components';
 import {fontFamily} from './var';
-import {theme} from './theme';
 
 injectGlobal`
   html,body {
-    font-family: Cereal, "PingFang SC", Roboto, system-ui, STHeiti, "Microsoft YaHei", system, -apple-system, ".SFNSDisplay-Regular", HelveticaNeue, LucidaGrande, "Hiragino Sans GB", sans-serif;
     -ms-text-size-adjust: 100%; // 2
     -webkit-text-size-adjust: 100%; // 2
     -webkit-font-smoothing: antialiased;
-    font-size: ${theme.baseFontSize}px;
   }
   body {
     margin: 0;
