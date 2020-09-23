637d0741b0d81154c7bc330f8fce19b926f5a71b
DrowsyFlesh <jjj201200@gmail.com>
2020-09-23 20:45:30 +0800

    修复自动宽屏失效

--- src/js/modules/videoWiden/UI/index.js
+++ src/js/modules/videoWiden/UI/index.js
@@ -21,7 +21,7 @@ export class VideoWidenUI extends UI {
         return new Promise(resolve => {
             const option = settings.subPage.value;
             this.setWide(option);
-            const player = document.querySelector('#bofqi');
+            const player = document.querySelector('#bofqi, #bilibiliPlayer');
             if (player) {
 
                 new MutationObserver((mutationList) => {
