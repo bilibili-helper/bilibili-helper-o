ac6ebf7d60c02fef0c73084eb7e470d1236d0112
memorydream <memorydream.chen@outlook.com>
2020-10-29 10:19:47 +0800

    fix：浏览器兼容性修复
    在edge中bilibili实际使用语言与插件识别语言不同的临时解决方案;
    修复在部分浏览器下，由于该模块与b站主页分区异步加载导致插件实际失效的bug

--- src/js/modules/partitionFilter/UI/index.js
+++ src/js/modules/partitionFilter/UI/index.js
@@ -16,29 +16,35 @@ export class PartitionFilterUI extends UI {
         });
     }
 
+    isTW = navigator.language === 'zh-TW';
+
+    getListBoxText = (cn, tw) => {
+        return this.isTW && tw ? tw : cn;
+    }
+
     keyToListBoxText = {
-        'life': '生活',
-        'live': '直播',
-        'ent': '娱乐',
-        'read': '专栏',
-        'game': '游戏',
-        'music': '音乐',
-        'dance': '舞蹈',
-        'anime': '番剧',
-        'douga': '动画',
-        'digital': '数码',
-        'movie': '电影',
-        'manga': '漫画',
-        'cheese': '课堂',
-        'kichiku': '鬼畜',
-        'fashion': '时尚',
-        'teleplay': 'TV剧',
-        'cinephile': '影视',
-        'guochuang': '国创',
-        'technology': '知识',
-        'information': '资讯',
-        'documentary': '纪录片',
-        'food': '美食',
+        'life': this.getListBoxText('生活'),
+        'live': this.getListBoxText('直播'),
+        'ent': this.getListBoxText('娱乐', '娛樂'),
+        'read': this.getListBoxText('专栏', '專欄'),
+        'game': this.getListBoxText('游戏', '遊戲'),
+        'music': this.getListBoxText('音乐', '音樂'),
+        'dance': this.getListBoxText('舞蹈'),
+        'anime': this.getListBoxText('番剧', '番劇'),
+        'douga': this.getListBoxText('动画', '動畫'),
+        'digital': this.getListBoxText('数码', '数位'),
+        'movie': this.getListBoxText('电影', '電影'),
+        'manga': this.getListBoxText('漫画', '漫畫'),
+        'cheese': this.getListBoxText('课堂', '課堂'),
+        'kichiku': this.getListBoxText('鬼畜'),
+        'fashion': this.getListBoxText('时尚', '時尚'),
+        'teleplay': this.getListBoxText('TV剧', 'TV劇'),
+        'cinephile': this.getListBoxText('影视', '影視'),
+        'guochuang': this.getListBoxText('国创', '國創'),
+        'technology': this.getListBoxText('知识', '知識'),
+        'information': this.getListBoxText('资讯', '資訊'),
+        'documentary': this.getListBoxText('纪录片', '紀錄片'),
+        'food': this.getListBoxText('美食'),
     };
 
     listBoxTextToItem = {};
@@ -50,19 +56,33 @@ export class PartitionFilterUI extends UI {
             let listBox = document.querySelectorAll('.item.sortable');
 
             if (listBox.length > 0) {
-                _.forEach(listBox, (item) => {
-                    this.listBoxTextToItem[item.innerText] = item;
+                this.attack(settings, listBox);
+            } else {                               //有些浏览器会在该脚本加载完成后才加载分区
+                var mo = new MutationObserver(() => {
+                    listBox =  document.querySelectorAll('.item.sortable');
+                    if (listBox.length > 0) {
+                        mo.disconnect();
+                        this.attack(settings, listBox);
+                    }
                 });
 
-                const filterList = _.filter(settings.subPage.options, (o) => o.on === true);
-                _.forEach(filterList, (v) => {
-                    this.filterMain(v.key);
-                    this.filterListBox(v.key);
-                });
+                mo.observe($('.list-box')[0].firstChild, {childList: true, subtree: true});
             }
             resolve();
         });
     };
+
+    attack = (settings ,listBox) => {
+        _.forEach(listBox, (item) => {
+            this.listBoxTextToItem[item.innerText] = item;
+        });
+
+        const filterList = _.filter(settings.subPage.options, (o) => o.on === true);
+        _.forEach(filterList, (v) => {
+            this.filterMain(v.key);
+            this.filterListBox(v.key);
+        });
+    }
 
     getFilterItemInListBox = (key) => {
         return this.listBoxTextToItem[this.keyToListBoxText[key]];
