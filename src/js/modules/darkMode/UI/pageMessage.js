25e7f10ca7c02ffd6335ae58acfd717cd56483ba
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2020-12-31 07:48:07 +0800

    深色模式 消息中心的消息设置（ .config .config-item ）的单选按钮（ label.type-selector.radio-selector ）的文字颜色 (#810)
    * darkMode: 消息中心 消息设置 单选按钮 文字颜色
    
    对于单选按钮（ .config .config-item .radio-selector ）：
    - 文字颜色为 var(--dark-font-0) 。

--- src/js/modules/darkMode/UI/pageMessage.js
+++ src/js/modules/darkMode/UI/pageMessage.js
@@ -289,6 +289,9 @@ const MessageDarkModeStyle = createGlobalStyle`
             &::before {
               background: var(--dark-1)!important;
             }
+            .radio-selector {
+              color: var(--dark-font-0);
+            }
           }
         }
       }
